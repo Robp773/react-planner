e83cef88695eaca6e5855d7a95422fbb041e6893
Rob <robpeterman573@gmail.com>
2019-01-04 23:00:34 -0500

    initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Main.css';
+
+export class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+        render(){
+            return(
+                <div>Main component</div>
+            )
+        }
+}
