fb2ee046def1cee339f54a2e40a874ba6be829ad
Rob <robpeterman573@gmail.com>
2019-01-09 23:48:48 -0500

    imported font awesome icons for top nav bar

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import Main from "./components/Main";
 import { Provider } from "react-redux";
 import store from "./store";
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMap, faCalendarWeek } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faMap, faCalendarWeek)
+
 ReactDOM.render(
  <Provider store={store}>
     <Main />
